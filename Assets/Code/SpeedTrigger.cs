6e4aff95ae726ee546a90d6182e61bff822c2984
amptkili-sys <amptkili@gmail.com>
2026-02-12 16:01:36 +0700

    Update SpeedTrigger.cs

--- Assets/Code/SpeedTrigger.cs
+++ Assets/Code/SpeedTrigger.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class SpeedTrigger : MonoBehaviour
 {
     public float speedFactor = 2.5f;
-    
+
     void OnTriggerEnter(Collider other)
     {
         other.GetComponent<FirstPersonMovement>().runSpeed *= speedFactor;
+        other.GetComponent<Jump>().jumpStrength = 10;
     }
 
     void OnTriggerExit(Collider other)
     {
         other.GetComponent<FirstPersonMovement>().runSpeed /= speedFactor;
+        other.GetComponent<Jump>().jumpStrength = 2;
     }
 
 }
