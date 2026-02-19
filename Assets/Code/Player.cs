d72868da91e428d15b00c5783e52015aca76c30e
amptkili-sys <amptkili@gmail.com>
2026-02-19 17:14:31 +0700

    M3Y6 атака файрбол и сбор монет

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -6,10 +6,27 @@ public class Player : MonoBehaviour
 {
     private int health = 10;
 
+    public GameObject fireballPrefab;
+    public Transform attackPoint;
+    int coins;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(fireballPrefab, attackPoint.position, attackPoint.rotation);
+        }
+    }
     // Метод, понижающий здоровье
     public void TakeDamage(int damage)
     {
         health -= damage;
         print("Здоровье игрока" + health);
+    }
+
+    public void CollectCoins()
+    {
+        coins++;
+        Debug.Log("Coins: " + coins);
     }
 }
